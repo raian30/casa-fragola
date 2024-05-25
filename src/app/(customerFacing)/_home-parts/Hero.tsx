53b60b56b27905718163dae877070b9eb263ba7a
Raian Melon <rainamelon11@gmail.com>
2024-05-25 21:14:58 +0200

    first commit

--- src/app/(customerFacing)/_home-parts/Hero.tsx
+++ src/app/(customerFacing)/_home-parts/Hero.tsx
@@ -4,7 +4,7 @@ import {CircleArrowDown} from "lucide-react";
 export default function Hero() {
     return (
         <div className={'bg-[#F5F5F5] flex flex-col items-start justify-start'}>
-            <div className={'relative px-4 md:px-8 lg:px-12 xl:px-20 2xl:px-28 h-[calc(100vh-120px)] w-screen'}>
+            <div className={'relative px-4 md:px-8 lg:px-12 xl:px-20 2xl:px-28 h-[calc(100dvh-120px)] w-screen'}>
                 <Image src={'/hero-image.png'} className={'object-cover !relative'} quality={100} alt={'Slika kuce'} fill />
                 <h1 className={'z-10 absolute bottom-0 sm:bottom-12 top-0 sm:top-auto right-0 sm:right-auto left-0 m-auto md:m-0 w-fit h-fit sm:left-12 md:left-20 lg:left-24 xl:left-44 2xl:left-60 text-[#FFF1DD] font-[200] md:font-[150] text-[2.5rem] md:text-[3.5rem] lg:text-[4.5rem] xl:text-[5rem] leading-normal lg:leading-[7rem] text-center sm:text-start'}>Udobnost<br/> Istarske Baštine</h1>
                 <div className={'z-10 absolute bottom-4 md:bottom-[4rem] right-12 md:right-20 lg:right-24 xl:right-44 2xl:right-60 text-[#FFF1DD] inline-flex gap-5 justify-center items-center'}>
