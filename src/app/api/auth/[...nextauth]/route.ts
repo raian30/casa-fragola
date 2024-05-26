848a7ea9a461a2ec459733be8a8c665d055d7f6f
Raian Melon <rainamelon11@gmail.com>
2024-05-26 18:52:51 +0200

    auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -31,6 +31,7 @@ const handler = NextAuth({
                     const AdminUser = {email: process.env.ADMIN_EMAIL as string, password: process.env.ADMIN_PASSWORD as string }
 
                     const comparePassword = await compare(password, AdminUser.password)
+                    console.log({ comparePassword });
 
                     if(AdminUser.email == email && comparePassword) {
                         console.log({ message: 'success' });
