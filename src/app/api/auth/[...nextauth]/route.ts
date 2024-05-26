ea86ee13d044bc7c0c49265f1305bcc7a6b534f9
Raian Melon <rainamelon11@gmail.com>
2024-05-26 18:39:19 +0200

    auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import {z} from "zod";
-import bcrypt from "bcrypt";
 
 const handler = NextAuth({
     session: {
