ccd18f3dbff14fbcf08407df95412debb5a3d8fe
Raian Melon <rainamelon11@gmail.com>
2024-05-28 21:06:32 +0200

    new sections

--- src/app/(customerFacing)/_home-parts/Hero.tsx
+++ src/app/(customerFacing)/_home-parts/Hero.tsx
@@ -4,7 +4,7 @@ import {CircleArrowDown} from "lucide-react";
 export default function Hero() {
     return (
         <>
-            <div className={'bg-[#F5F5F5] relative px-4 md:px-8 lg:px-12 xl:px-20 2xl:px-28 h-[calc(100dvh-20px)] w-screen'}>
+            <section className={'bg-[#F5F5F5] relative px-4 md:px-8 lg:px-12 xl:px-20 2xl:px-28 h-[calc(100dvh-20px)] w-screen'}>
                 <Image loading={'eager'} src={'/hero-image.png'} className={'object-cover !relative'} quality={100} alt={'Slika kuce'}
                        fill/>
                 <h1 className={'z-10 absolute bottom-0 sm:bottom-12 top-0 sm:top-auto right-0 sm:right-auto left-0 m-auto md:m-0 w-fit h-fit sm:left-12 md:left-20 lg:left-24 xl:left-44 2xl:left-60 text-[#FFF1DD] font-[200] md:font-[150] text-[2.5rem] md:text-[3.5rem] lg:text-[4.5rem] xl:text-[5rem] leading-normal lg:leading-[7rem] text-center sm:text-start'}>Udobnost<br/> Istarske
@@ -14,12 +14,12 @@ export default function Hero() {
                     <p className={'sm:block hidden'}>Pogledajte više</p>
                     <CircleArrowDown strokeWidth={1} size={40}/>
                 </div>
-            </div>
-            <div className={'bg-[#F5F5F5] w-screen px-12 md:px-20 lg:px-24 xl:px-44 2xl:px-60 py-20'}>
+            </section>
+            <section className={'bg-[#F5F5F5] w-screen px-12 md:px-20 lg:px-24 xl:px-44 2xl:px-60 py-20'}>
                 <h2 className={'text-xl md:text-[1.4rem] font-light'}>Želite li miran i opušten odmor
                     daleko od gužve, ali i blizu događanja, zabave, ukusne hrane i vrhunskih vina koje Istra nudi?
                     Ovdje možete pronaći savršen spoj odmora i uživanja u svemu što Istra pruža!</h2>
-            </div>
+            </section>
         </>
     )
 }
--- src/app/(customerFacing)/_home-parts/Second.tsx
+++ src/app/(customerFacing)/_home-parts/Second.tsx
@@ -1,7 +1,59 @@
+import Image from "next/image";
+import {FilledLink} from "@/app/(customerFacing)/_components/Buttons";
+import {Bed, Flame, ParkingSquare, Wifi, Wind} from "lucide-react";
+
 export default function Second() {
     return(
-        <div className={'w-screen h-20'}>
-
-        </div>
+        <section className={'px-4 md:px-8 lg:px-12 xl:px-20 2xl:px-28 w-screen my-20 flex flex-col gap-20'}>
+            <div
+                className={'bg-[#EAD7E6] gap-16 lg:gap-0 flex flex-col lg:flex-row items-center justify-center lg:justify-between px-8 lg:px-0 pt-12 md:pt-16 2xl:pt-28 pb-12 md:pb-16 2xl:pb-28 lg:pl-12 xl:pl-20 2xl:pl-28'}>
+                <div className={'w-full h-full lg:w-1/2'}>
+                    <Image src={'/image2.png'} alt={'House picture 1'} width={760} height={505}
+                           className={'w-full h-full object-cover'}/>
+                </div>
+                <div
+                    className={'w-full lg:w-1/2 text-center px-5 lg:px-10 xl:px-20 2xl:px-52 flex flex-col items-center justify-center gap-10'}>
+                    <h1 className={'text-4xl xl:text-5xl'}>Fragola and white</h1>
+                    <p className={'text-lg font-light'}>Do you want to enjoy your holiday in peace and sepration from
+                        the rest of the world, and yet be close to events and entertainment and gastronomic and
+                        oenological hedonism</p>
+                    <FilledLink href={'/'} className={'uppercase'}>Rezervirajte sada</FilledLink>
+                </div>
+            </div>
+            <div
+                className={'bg-[#EDE8B8] gap-16 lg:gap-0 flex flex-col lg:flex-row items-center justify-center lg:justify-between px-8 lg:px-0 pt-12 md:pt-16 2xl:pt-28 pb-12 md:pb-16 2xl:pb-28 lg:pr-12 xl:pr-20 2xl:pr-28'}>
+                <div
+                    className={'w-full lg:w-1/2 text-center px-5 lg:px-10 xl:px-20 2xl:px-52 flex flex-col items-center justify-center gap-10'}>
+                    <h1 className={'text-4xl xl:text-5xl'}>Olive and Fragola</h1>
+                    <p className={'text-lg font-light'}>Do you want to enjoy your holiday in peace and sepration from
+                        the rest of the world, and yet be close to events and entertainment and gastronomic and
+                        oenological hedonism</p>
+                    <FilledLink href={'/'} className={'uppercase'}>Rezervirajte sada</FilledLink>
+                </div>
+                <div className={'w-full h-full lg:w-1/2'}>
+                    <Image src={'/image2.png'} alt={'House picture 2'} width={760} height={505}
+                           className={'w-full h-full object-cover'}/>
+                </div>
+            </div>
+            <div
+                className={'gap-32 xl:gap-40 flex flex-col lg:flex-row items-center justify-between px-8 lg:px-0 pt-12 md:pt-16 2xl:pt-28 pb-12 md:pb-16 2xl:pb-28'}>
+                <div
+                    className={'w-full lg:w-1/2 px-0 lg:px-10 xl:px-20 2xl:px-28 flex flex-col items-start justify-center gap-10'}>
+                    <h1 className={'text-4xl xl:text-5xl'}>Sadržaj</h1>
+                    <p className={'text-lg font-light'}>Do you want to enjoy your holiday in peace and sepration from
+                        the rest of the world, and yet be close to events and entertainment and gastronomic and
+                        oenological hedonism</p>
+                    <FilledLink href={'/'} className={'uppercase'}>Pogledajte sve značajke</FilledLink>
+                </div>
+                <div className={'w-full h-full lg:w-1/2 grid grid-cols-2 gap-2'}>
+                    <div className={'flex gap-2'}><Bed/> <p>3 beds (1 double and 2 single)</p></div>
+                    <div className={'flex gap-2'}><Image src={'/pool.svg'} width={23} height={23} alt={'pool'}/> <p>Outdoor swimming pool</p></div>
+                    <div className={'flex gap-2'}><Wifi/> <p>Free WI-FI</p></div>
+                    <div className={'flex gap-2'}><ParkingSquare/> <p>Free Parking</p></div>
+                    <div className={'flex gap-2'}><Wind/> <p>Air conditioning</p></div>
+                    <div className={'flex gap-2'}><Flame/> <p>BBQ facilities</p></div>
+                </div>
+            </div>
+        </section>
     )
 }
