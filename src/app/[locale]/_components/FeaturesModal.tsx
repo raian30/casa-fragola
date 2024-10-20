4652710ef2a258f9db4887a6fcc9664e1957e5ce
Raian <raianmelon11@gmail.com>
2024-10-20 22:06:22 +0200

    finished modals, added translations

--- src/app/[locale]/_components/FeaturesModal.tsx
+++ src/app/[locale]/_components/FeaturesModal.tsx
@@ -1,6 +1,19 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { X } from "lucide-react";
+import {
+    Bath,
+    Bed,
+    Check,
+    ChefHat, Ellipsis,
+    Flower,
+    MessageSquare,
+    ParkingCircle,
+    Tv,
+    Utensils,
+    Waves,
+    Wifi,
+    X
+} from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const Modal = () => {
@@ -21,7 +34,8 @@ const Modal = () => {
         }, 300);
     };
 
-    const t = useTranslations('Second');
+    const t = useTranslations('Third');
+    const t2 = useTranslations('FeaturesModal');
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -46,36 +60,97 @@ const Modal = () => {
 
     return (
         <>
-            <button onClick={openModal}
-                    className={'bg-white hover:bg-gray-100 transition-all px-8 py-3 border border-black font-light uppercase w-fit text-sm'}>
-                {t('btn-text-3')}
-            </button>
-            {isOpen && (
-                <div
-                    className={`fixed z-50 inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
+        <button onClick={openModal}
+                className={'bg-white hover:bg-gray-100 transition-all px-8 py-3 border border-black font-light uppercase w-fit'}>
+            {t('btn-text-1')}
+        </button>
+    {isOpen && (
+        <div
+            className={`fixed z-50 inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
+            onClick={closeModal}
+        >
+            <div
+                className={`w-screen h-[calc(100vh-7rem)] overflow-y-scroll mx-10 md:mx-14 lg:mx-12 xl:mx-[5.5rem] 2xl:mx-[7.5rem] bg-white shadow-lg relative px-5 lg:p-14 transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
+                onClick={(e) => e.stopPropagation()}
+            >
+                <button
                     onClick={closeModal}
+                    className="absolute top-5 lg:top-14 right-5 lg:right-14 hover:text-gray-800"
                 >
-                    <div
-                        className={`w-screen h-[calc(100vh-7rem)] mx-10 md:mx-14 lg:mx-12 xl:mx-[5.5rem] 2xl:mx-[7.5rem] bg-white shadow-lg relative p-14 transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        <button
-                            onClick={closeModal}
-                            className="absolute top-14 right-14 hover:text-gray-800"
-                        >
-                            <X size={30} />
-                        </button>
-                        <h2 className="text-3xl">Everything for Your Perfect Vacation</h2>
-                        <div className={'my-10'}>
-                            <h1>Here goes features from booking</h1>
-                        </div>
-                        <button onClick={closeModal}
-                                className={'bg-white hover:bg-gray-100 transition-all px-8 py-3 border border-black font-light uppercase absolute bottom-14 left-0 right-0 mx-auto w-fit'}>
-                            Close
-                        </button>
+                    <X size={30} />
+                </button>
+                    <h2 className="text-2xl sm:text-3xl mt-5 lg:mt-0">{t2('sve-za-vas')} <br className={'sm:hidden'}/> {t2('savsen-odmor')}</h2>
+                    <div className={'my-10 grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-14'}>
+                    <div className={'flex flex-col gap-10'}>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><ParkingCircle strokeWidth={1.5}/> {t2('parking')}</h1>
+                    <p className={'text-sm flex gap-2'}>{t2('parking-1')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Wifi strokeWidth={1.5}/> {t2('internet')}</h1>
+                    <p className={'text-sm'}>{t2('internet-1')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><ChefHat strokeWidth={1.5}/> {t2('kuhinja')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('kuhinja-1')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-2')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-3')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-4')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-5')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-6')}</p>
+                    <p className={'text-sm flex gap-2'}> <Check strokeWidth={1.5}/>{t2('kuhinja-7')}</p>
+                    </div>
+                    </div>
+                    <div className={'flex flex-col gap-10'}>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}> <Bed strokeWidth={1.5}/>{t2('spavaca-soba')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('spavaca-soba-1')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}> <Bath strokeWidth={1.5}/> {t2('kupaonica')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('kupaonica-1')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('kupaonica-2')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('kupaonica-3')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Tv strokeWidth={1.5}/> {t2('mediji')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('mediji-1')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Flower strokeWidth={1.5}/> {t2('dvoriste')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('dvoriste-1')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('dvoriste-2')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('dvoriste-3')}</p>
+                    </div>
+                    </div>
+                    <div className={'flex flex-col gap-10'}>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Waves strokeWidth={1.5}/> {t2('bazen')}</h1>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Utensils strokeWidth={1.5}/> {t2('hrana')}
+                    </h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('hrana-1')}
+                    kave/čaja</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><MessageSquare strokeWidth={1.5}/> {t2('usluga')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('usluga-1')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('usluga-2')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('usluga-3')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('usluga-4')}</p>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                    <h1 className={'font-medium flex gap-2'}><Ellipsis strokeWidth={1.5}/> {t2('ostalo')}</h1>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('ostalo-1')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('ostalo-2')}</p>
+                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> {t2('ostalo-3')}</p>
+                    </div>
+                    </div>
                     </div>
                 </div>
-            )}
+                </div>
+                )}
         </>
     );
 };
--- src/app/[locale]/_components/RulesModal.tsx
+++ src/app/[locale]/_components/RulesModal.tsx
@@ -1,6 +1,19 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { X } from "lucide-react";
+import {
+    Bath,
+    Bed, Bone,
+    Check,
+    ChefHat, CigaretteOff, CreditCard, Download, Ellipsis,
+    Flower, Info,
+    MessageSquare, Moon,
+    ParkingCircle, PartyPopper, PawPrint, PersonStanding,
+    Tv, Users,
+    Utensils,
+    Waves,
+    Wifi,
+    X
+} from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const Modal = () => {
@@ -22,6 +35,7 @@ const Modal = () => {
     };
 
     const t = useTranslations('Third');
+    const t2 = useTranslations('RulesModal');
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -56,23 +70,88 @@ const Modal = () => {
                     onClick={closeModal}
                 >
                     <div
-                        className={`w-screen h-[calc(100vh-7rem)] mx-10 md:mx-14 lg:mx-12 xl:mx-[5.5rem] 2xl:mx-[7.5rem] bg-white shadow-lg relative p-14 transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
+                        className={`w-screen h-[calc(100vh-7rem)] overflow-y-scroll mx-10 md:mx-14 lg:mx-12 xl:mx-[5.5rem] 2xl:mx-[7.5rem] bg-white shadow-lg relative px-5 lg:p-14 transition-opacity duration-300 ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
                         onClick={(e) => e.stopPropagation()}
                     >
                         <button
                             onClick={closeModal}
-                            className="absolute top-14 right-14 hover:text-gray-800"
+                            className="absolute top-5 lg:top-14 right-5 lg:right-14 hover:text-gray-800"
                         >
                             <X size={30} />
                         </button>
-                        <h2 className="text-3xl">House rules</h2>
-                        <div className={'my-10'}>
-                            <h1>Here goes house rules from booking</h1>
+                        <h2 className="text-2xl sm:text-3xl mt-5 lg:mt-0">{t2("pravila-kuce")}</h2>
+                        <div className={'my-10 grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-14'}>
+                            <div className={'flex flex-col gap-10'}>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Download className={'-rotate-90'} strokeWidth={1.5}/> {t2("prijava")}</h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("prijava-1")}<br/>
+                                        {t2("prijava-2")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Download className={'rotate-90'} strokeWidth={1.5}/> {t2("odjava")}</h1>
+                                    <p className={'text-sm'}>{t2("odjava-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Info strokeWidth={1.5}/> {t2("otkazivanje")}</h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("otkazivanje-1")} <br/> {t2("otkazivanje-2")}</p>
+                                </div>
+                            </div>
+                            <div className={'flex flex-col gap-10'}>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Info strokeWidth={1.5}/>{t2("polog")}</h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("polog-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Users strokeWidth={1.5}/> {t2("djeca")}
+                                    </h1>
+                                    <p className={'text-sm flex gap-2 font-bold'}>{t2("djeca-1")}</p>
+                                    <p className={'text-sm flex gap-2'}>{t2("djeca-2")}<br/>
+                                        {t2("djeca-3")}</p>
+                                    <p className={'text-sm flex gap-2 font-bold'}>{t2("djeca-4")}</p>
+                                    <p className={'text-sm flex gap-2'}>{t2("djeca-5")}<br/>
+                                        {t2("djeca-6")}<br/>
+
+                                        {t2("djeca-7")}
+
+                                    </p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><PersonStanding strokeWidth={1.5}/> {t2("granica")}</h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("granica-1")}</p>
+                                </div>
+                            </div>
+                            <div className={'flex flex-col gap-10'}>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><CreditCard strokeWidth={1.5}/> {t2("kartice")}</h1>
+                                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> American Express</p>
+                                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> Visa</p>
+                                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> MasterCard</p>
+                                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> Diners Club</p>
+                                    <p className={'text-sm flex gap-2'}><Check strokeWidth={1.5}/> Maestro</p>
+                                    <p className={'text-sm flex gap-2'}><X strokeWidth={1.5}/> {t2("kartice-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><CigaretteOff strokeWidth={1.5}/> {t2("pusenje")}
+                                    </h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("pusenje-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><PartyPopper strokeWidth={1.5}/> {t2("zabave")}
+                                    </h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("zabave-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><Moon strokeWidth={1.5}/> {t2("red")}
+                                    </h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("red-1")}</p>
+                                </div>
+                                <div className={'flex flex-col gap-2'}>
+                                    <h1 className={'font-medium flex gap-2'}><PawPrint strokeWidth={1.5}/> {t2("ljubimci")}
+                                    </h1>
+                                    <p className={'text-sm flex gap-2'}>{t2("ljubimci-1")}</p>
+                                </div>
+                            </div>
                         </div>
-                        <button onClick={closeModal}
-                                className={'bg-white hover:bg-gray-100 transition-all px-8 py-3 border border-black font-light uppercase absolute bottom-14 left-0 right-0 mx-auto w-fit'}>
-                            Close
-                        </button>
                     </div>
                 </div>
             )}
